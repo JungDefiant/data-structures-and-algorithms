292ebd92c303c10805323b6c080a120ad6e5ff7a
JungDefiant <jungdefiant@gmail.com>
2020-07-17 14:44:58 -0700

    Added Node and Stack classes. Added Peek, Pop, and Push methods.

---
+++ data-structures/stacks-and-queues/StacksAndQueues/Classes/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    internal class Stack
+    {
+        public Node Top { get; set; }
+
+        public void Push(int value)
+        {
+            Node node = new Node(value, Top);
+            Top = node;
+        }
+
+        public Node Pop()
+        {
+            try
+            {
+                Node temp = Top;
+                Top = Top.Next;
+                return temp;
+            }
+            catch(NullReferenceException)
+            {
+                return null;
+            }
+        }
+
+        public int Peek()
+        {
+            try
+            {
+                return Top.Value;
+            }
+            catch(NullReferenceException)
+            {
+                return 0;
+            }
+        }
+    }
+}
