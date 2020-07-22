57d9f48f9c8a349ccacad2bc9273a1b00b36b70b
JungDefiant <jungdefiant@gmail.com>
2020-07-22 08:41:47 -0700

    Attempt 2 push

---
+++ code-challenges/fifo-animal-shelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace FIFOAnimalShelter.Classes
+{
+    public class AnimalShelter
+    {
+        public Animal First { get; set; }
+        public Animal Rear { get; set; }
+
+        public void Enqueue(Animal animal)
+        {
+            Animal temp = First;
+            if (First == null) Rear = animal;
+            First = animal;
+            First.Next = temp;
+        }
+
+        public Animal Dequeue(string pref)
+        {
+            try
+            {
+                Animal dequeuedAnimal = Rear;
+
+                if (dequeuedAnimal.Name.Equals(pref))
+                {
+                    Rear = Rear.Next;
+                    dequeuedAnimal.Next = null;
+
+                    return dequeuedAnimal;
+                }
+                else return null;
+            }
+            catch(NullReferenceException)
+            {
+                throw new NullReferenceException();
+            }
+        }
+    }
+}
---
+++ code-challenges/fifo-animal-shelter/FIFOAnimalShelter/Classes/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter.Classes
+{
+    public class Dog : Animal
+    {
+        public bool HasSpots { get; set; }
+
+        public Dog()
+        {
+            Name = "Dog";
+        }
+    }
+}
