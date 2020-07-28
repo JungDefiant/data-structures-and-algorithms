7c9d6e59c56f1c516c9c6261ec3f01a0f90c7033
JungDefiant <jungdefiant@gmail.com>
2020-07-27 22:29:33 -0700

    Created Tree classes

---
+++ data-structures/tree/Trees/BinarySearchTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    public class BinarySearchTree : Tree
+    {
+        public void Add(int value)
+        {
+            Node newNode = new Node(value, null, null);
+            Node currentNode = Root;
+
+            while(currentNode != null)
+            {
+                if(newNode.Value < currentNode.Value)
+                {
+                    if (currentNode.Left == null)
+                    {
+                        currentNode.Left = newNode;
+                        currentNode = null;
+                    }
+                    else currentNode = currentNode.Left;
+                }
+                else
+                {
+                    if (currentNode.Right == null)
+                    {
+                        currentNode.Right = newNode;
+                        currentNode = null;
+                    }
+                    else currentNode = currentNode.Right;
+                }
+            }
+        }
+
+        public bool Contains(int value)
+        {
+            Node currentNode = Root;
+
+            while (currentNode != null)
+            {
+                if (value < currentNode.Value)
+                {
+                    currentNode = currentNode.Left;
+                }
+                else if (value > currentNode.Value)
+                {
+                    currentNode = currentNode.Right;
+                }
+                else return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ data-structures/tree/Trees/Tree.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    public class Tree
+    {
+        public Node Root { get; set; }
+
+        public int[] TraversePreOrder()
+        {
+            List<int> values = new List<int>();
+
+            TraversePreOrder(Root, values);
+
+            return values.ToArray();
+        }
+
+        public void TraversePreOrder(Node root, List<int> list)
+        {
+            list.Add(root.Value);
+
+            while (root.Left != null)
+            {
+                TraversePreOrder(root.Left, list);
+            }
+
+            while (root.Right != null)
+            {
+                TraversePreOrder(root.Right, list);
+            }
+        }
+
+        public int[] TraverseInOrder()
+        {
+            List<int> values = new List<int>();
+
+            TraverseInOrder(Root, values);
+
+            return values.ToArray();
+        }
+
+        public void TraverseInOrder(Node root, List<int> list)
+        {
+            while (root.Left != null)
+            {
+                list.Add(root.Left.Value);
+                TraverseInOrder(root.Left, list);
+            }
+
+            list.Add(root.Value);
+
+            while (root.Right != null)
+            {
+                list.Add(root.Right.Value);
+                TraverseInOrder(root.Right, list);
+            }
+        }
+
+        public int[] TraversePostOrder()
+        {
+            List<int> values = new List<int>();
+
+            TraversePostOrder(Root, values);
+
+            return values.ToArray();
+        }
+
+        public void TraversePostOrder(Node root, List<int> list)
+        {
+            while (root.Left != null)
+            {
+                list.Add(root.Left.Value);
+                TraversePostOrder(root.Left, list);
+            }
+
+            while (root.Right != null)
+            {
+                list.Add(root.Right.Value);
+                TraversePostOrder(root.Right, list);
+            }
+
+            list.Add(root.Value);
+        }
+    }
+}
