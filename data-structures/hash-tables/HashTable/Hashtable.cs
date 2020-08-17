f203729e8893ec0e0ab117618c83063fd184abec
JungDefiant <jungdefiant@gmail.com>
2020-08-17 08:16:10 -0700

    Challenge 30 - initial commit

---
+++ data-structures/hash-tables/HashTable/Hashtable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace HashTable
+{
+    public class Hashtable<T>
+    {
+        public LinkedList<Node<T>>[] Map { get; set; }
+
+        public Hashtable(int size)
+        {
+            Map = new LinkedList<Node<T>>[size];
+        }
+
+
+    }
+}
---
+++ data-structures/hash-tables/HashtableTest/HashtableTest.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+
+namespace HashtableTest
+{
+    public class HashtableTest
+    {
+        [Fact]
+        public void CheckAddKeyValue()
+        {
+
+        }
+
+        [Fact]
+        public void CheckRetrieveValue()
+        {
+
+        }
+
+        [Fact]
+        public void CheckNullKey()
+        {
+
+        }
+
+        [Fact]
+        public void CheckHandleCollision()
+        {
+
+        }
+
+        [Fact]
+        public void CheckRetrieveValueWithCollision()
+        {
+
+        }
+
+        [Fact]
+        public void CheckSuccessfulKeyHash()
+        {
+
+        }
+    }
+}
