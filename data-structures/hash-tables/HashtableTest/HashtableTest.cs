359cc61bbfeff14b5913e6d4e20989db5e3ec2d0
JungDefiant <jungdefiant@gmail.com>
2020-08-17 08:28:12 -0700

    CH 30 - added unit tests

--- data-structures/hash-tables/HashtableTest/HashtableTest.cs
+++ data-structures/hash-tables/HashtableTest/HashtableTest.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using HashTable;
 
 namespace HashtableTest
 {
@@ -8,37 +9,61 @@ namespace HashtableTest
         [Fact]
         public void CheckAddKeyValue()
         {
+            Hashtable<int> table = new Hashtable<int>(3);
 
+            table.Add("number", 3);
+
+            Assert.True(table.Get("number") == 3);
         }
 
         [Fact]
         public void CheckRetrieveValue()
         {
+            Hashtable<int> table = new Hashtable<int>(3);
 
+            table.Add("number", 3);
+
+            Assert.True(table.Get("number") == 3);
         }
 
         [Fact]
         public void CheckNullKey()
         {
+            Hashtable<int> table = new Hashtable<int>(3);
 
+            Assert.Null(table.Get("number"));
         }
 
         [Fact]
         public void CheckHandleCollision()
         {
+            Hashtable<int> table = new Hashtable<int>(3);
 
+            table.Add("number", 3);
+            table.Add("number", 5);
+            table.Add("number", 7);
+
+            Assert.NotNull(table.Get("number"));
         }
 
         [Fact]
         public void CheckRetrieveValueWithCollision()
         {
+            Hashtable<int> table = new Hashtable<int>(3);
 
+            table.Add("number", 3);
+            table.Add("number", 5);
+            table.Add("number", 7);
+
+            Assert.True(table.Get("number") == 3);
         }
 
         [Fact]
         public void CheckSuccessfulKeyHash()
         {
+            Hashtable<int> table = new Hashtable<int>(3);
 
+            Assert.True(table.Hash("number") == 3);
         }
     }
 }
